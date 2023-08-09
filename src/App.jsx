24e1c47383ee9e30da74088c1e20455061eb1c11
salahuddin8544 <salahuddinahmed8544@gmail.com>
2023-08-09 06:56:29 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import video from './assets/video/background.mp4'
+import './App.css'
+import { RouterProvider } from 'react-router-dom'
+import router from './Routes/Route'
+
+function App() {
+
+
+  return (
+    <div className=''>
+      <video src={video} autoPlay loop className='video-background' muted playsInline>
+       
+    
+      </video>
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  )
+}
+
+export default App
